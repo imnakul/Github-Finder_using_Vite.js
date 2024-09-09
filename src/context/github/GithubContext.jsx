ceb51114a32cd8f0d0eef816667ea27a05e46649
Nakul Srivastava <imnakul44@gmail.com>
2024-09-09 11:55:09 +0530

    Added Reducers and usereducer hooks

--- src/context/github/GithubContext.jsx
+++ src/context/github/GithubContext.jsx
@@ -1,13 +1,18 @@
 import { createContext } from "react";
-import { useEffect, useState } from "react";
+import { useEffect, useReducer } from "react";
+import githubReducer from "./GithubReducer";
 
 const GithubContext = createContext();
 const GITHUB_URL = "https://api.github.com";
 const GITHUB_TOKEN = "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";
 
 export const GithubProvider = ({ children }) => {
-   const [users, setUsers] = useState([]);
-   const [loading, setLoading] = useState(true);
+   const intitalState = {
+      users: [],
+      loading: true,
+   };
+
+   const [state, dispatch] = useReducer(githubReducer, intitalState);
 
    const fetchUsers = async () => {
       const response = await fetch(GITHUB_URL + "/users", {
@@ -17,12 +22,13 @@ export const GithubProvider = ({ children }) => {
       });
 
       const data = await response.json();
-      setUsers(data);
-      setLoading(false);
+      dispatch({ type: "GET_USERS", payload: data });
    };
 
    return (
-      <GithubContext.Provider value={{ users, loading, fetchUsers }}>
+      <GithubContext.Provider
+         value={{ users: state.users, loading: state.loading, fetchUsers }}
+      >
          {children}
       </GithubContext.Provider>
    );
