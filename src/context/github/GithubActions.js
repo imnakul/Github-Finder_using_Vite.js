d285b4d5268aa8803b1a9834363b057eebd4648d
Nakul Srivastava <imnakul44@gmail.com>
2024-09-22 22:23:43 +0530

    Refactoring_UsingAXIOSnow_insteadofFETCH + Combined Functions getSIngleUser and GetUSERRepos

--- src/context/github/GithubActions.js
+++ src/context/github/GithubActions.js
@@ -1,5 +1,14 @@
+import axios from "axios";
 const GITHUB_URL = "https://api.github.com";
 const GITHUB_TOKEN = "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";
+
+//Instance of Axios
+const github = axios.create({
+   baseURL: GITHUB_URL,
+   headers: {
+      Authorization: GITHUB_TOKEN,
+   },
+});
 
 // Get search results
 export const searchUsers = async (text) => {
@@ -7,48 +16,16 @@ export const searchUsers = async (text) => {
       q: text,
    });
 
-   const response = await fetch(`${GITHUB_URL}/search/users?${params}`, {
-      headers: {
-         Authorization: GITHUB_TOKEN,
-      },
-   });
-
-   const { items } = await response.json();
-   return items;
+   const response = await github.get(`/search/users?${params}`); //returns in json also, in object called Data.
+   return response.data.items;
 };
 
-//Get single user
-export const getUser = async (login) => {
-   const response = await fetch(`${GITHUB_URL}/users/${login}`, {
-      headers: {
-         Authorization: GITHUB_TOKEN,
-      },
-   });
-
-   if (response.status === 400) {
-      window.location = "/notfound";
-   } else {
-      const data = await response.json();
-      return data;
-   }
-};
-
-//Get User Repos
-export const getUserRepos = async (login) => {
-   const params = new URLSearchParams({
-      sort: "created",
-      per_page: 10,
-   });
-
-   const response = await fetch(
-      `${GITHUB_URL}/users/${login}/repos?${params}`,
-      {
-         headers: {
-            Authorization: GITHUB_TOKEN,
-         },
-      }
-   );
-
-   const data = await response.json();
-   return data;
+//Get Single User and Repos
+export const getUserAndRepos = async (login) => {
+   //now to send request to two diff URLS, we will use Promise all and send an array of Request
+   const [user, repos] = await Promise.all([
+      github.get(`/users/${login}`),
+      github.get(`/users/${login}/repos`),
+   ]);
+   return { user: user.data, repos: repos.data };
 };
